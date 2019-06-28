41153067d45c26c31e3752890245a2418cbdec75
Peter Willmot <peter@Qri.ie>
2019-06-28 03:00:41 +0100

    IsValidEMailAddress added

--- CLRSample-VSSolution/CLRSample/GetFastTokenLength.cs
+++ CLRSample-VSSolution/CLRSample/GetFastTokenLength.cs
@@ -4,12 +4,34 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 
+using System.Text.RegularExpressions;
+
 public partial class UserDefinedFunctions
 {
     [SqlFunction(DataAccess = DataAccessKind.None)]
-    public static int GetFastTokenLength(string list, int element)
+    public static int GetFastTokenLength(string list, char delimiter, int tokenOrdinal)
     {
-        return list.Split('|')[element].Length;
+        if (!String.IsNullOrEmpty(list) && tokenOrdinal >= 0)
+        {
+            string[] tokens = list.Split(new char[] { delimiter });
+
+            if (tokens != null && tokens.Length > tokenOrdinal) return tokens[tokenOrdinal].Length;
+        }
+
+        return -1;
+    }
+
+    private static readonly Regex _eMailValidator = new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$", RegexOptions.IgnoreCase);
+
+    [SqlFunction(DataAccess = DataAccessKind.None)]
+    public static bool IsValidEMailAddress(string emailAddress)
+    {
+        if (!String.IsNullOrEmpty(emailAddress))
+        {
+            return _eMailValidator.IsMatch(emailAddress);
+        }
+
+        return false;
     }
 };
 
