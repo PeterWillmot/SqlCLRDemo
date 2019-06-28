fe91cf6f7e471f43749120f6c812cfe8995557a6
Peter Willmot <peter@Qri.ie>
2019-06-28 03:05:51 +0100

    clr strict security commented

--- T-SQL/02-Setup-StringTokenLength-CLR.sql
+++ T-SQL/02-Setup-StringTokenLength-CLR.sql
@@ -3,7 +3,7 @@ GO
 
 EXEC sp_configure 'show advanced options', 1
 EXEC sp_configure 'clr enabled', 1
---EXEC sp_configure 'clr strict security', 0
+--EXEC sp_configure 'clr strict security', 0     -- required from SQL2019 (but not necessarily a good idea!)
 RECONFIGURE
 
 USE Demo
